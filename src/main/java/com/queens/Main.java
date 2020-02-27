76420ea4a18e9af52d559608e1679d94140905f9
ediril <naotilus@gmail.com>
2020-02-27 01:56:52 -0500

    Implement diagonal checking

--- src/main/java/com/queens/Main.java
+++ src/main/java/com/queens/Main.java
@@ -3,7 +3,9 @@ package com.queens;
 import org.apache.commons.collections4.iterators.PermutationIterator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -31,6 +33,42 @@ public class Main {
             // to generate all permutations of one queen per row & col
             PermutationIterator<Integer> permIterator = new PermutationIterator<>(columns);
 
+            // Since we are using a permutation algorithm on column id's, there will be
+            // exactly one queen per row and per column. So, we just need to check
+            // attacks diagonally and the no-straight-line constraint
+            //
+            // Left to right diagonals: col# - row#
+            //  0  1  2  3
+            // -1  0  1  2
+            // -2 -1  0  1
+            // -3 -2 -1  0
+            //
+            // Right to left diagonals: col# + row#
+            //  0  1  2  3
+            //  1  2  3  4
+            //  2  3  4  5
+            //  3  4  5  6
+            Set<Integer> rightDiagonals = new HashSet<>(N);
+            Set<Integer> leftDiagonals = new HashSet<>(N);
+
+            while (permIterator.hasNext()) {
+                List<Integer> permutation = permIterator.next();
+
+                leftDiagonals.clear();
+                rightDiagonals.clear();
+                for (int row=0; row < N; row++) {
+                    int col = permutation.get(row);
+                    leftDiagonals.add(col + row);
+                    rightDiagonals.add(col - row);
+                }
+
+                // If there is one of each column id's in each set, it means
+                // each queen is on its own diagonal so none can attack another
+                if (leftDiagonals.size() == N &&
+                    rightDiagonals.size() == N) {
+                    System.out.println(permutation);
+                }
+            }
         }
         catch (NumberFormatException e) {
             System.out.println("ERROR: Invalid input argument, N must be integer");
