7dd9670f2513376b4ca13f7ab902b43bc9b6b1ff
ediril <naotilus@gmail.com>
2020-03-01 04:38:32 -0500

    Add cli option to select which algorithm to use

--- src/main/java/com/queens/Main.java
+++ src/main/java/com/queens/Main.java
@@ -1,24 +1,29 @@
 package com.queens;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main {
 
     public static void main(String[] args) {
         ArgumentParser parser = ArgumentParsers.newFor("queen_placer").build();
-        parser.addArgument("board_size").type(Integer.class);
+        parser.addArgument("board_size").type(Integer.class).required(true);
+        parser.addArgument("-p", "--permuting").action(Arguments.storeConst()).setConst(true).setDefault(false);
 
         try {
-            int N = parser.parseArgs(args).get("board_size");
+            Namespace ns = parser.parseArgs(args);
+            int N = ns.get("board_size");
 
             if (N == 0) {
                 System.out.println("Invalid board_size, must be greater than zero");
                 System.exit(1);
             }
 
-            QueenPlacer placer = new PermutingQueenPlacer(N);
+            QueenPlacer placer = ns.get("permuting") ?
+                    new PermutingQueenPlacer(N) : new EliminatingQueenPlacer(N);
 
             Result result = placer.findSolutions(solution -> {
                 System.out.println(QueenPlacer.solutionToAsciiBoard(solution));
