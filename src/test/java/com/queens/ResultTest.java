9b4c23e04f6ab694302e7c1104c1578474b7d865
ediril <naotilus@gmail.com>
2020-03-01 01:30:55 -0500

    Add @Test annotation

--- src/test/java/com/queens/ResultTest.java
+++ src/test/java/com/queens/ResultTest.java
@@ -11,11 +11,13 @@ public class ResultTest {
         assertEquals("No solutions found (0 possibilities evaluated)", new Result(0, 0).report());
     }
 
+    @Test
     public void reportOne() {
         assertEquals("1 solution found (10 possibilities evaluated)", new Result(1, 10).report());
     }
 
+    @Test
     public void reportLots() {
-        assertEquals("10 solution found (100 possibilities evaluated)", new Result(10, 100).report());
+        assertEquals("10 solutions found (100 possibilities evaluated)", new Result(10, 100).report());
     }
 }
